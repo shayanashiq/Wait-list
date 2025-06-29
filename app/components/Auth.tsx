ee375b78961a2844f85b5457a5c415b260909d7c
shayanashiq <shayanashiq8@gmil.com.com>
2025-06-30 03:03:10 +0500

    Newenv

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -14,7 +14,7 @@ const onHandle = async () => {
     return;
   }
   try {
-    const res = await axios.post('https://waitinglist-9u8uwm43j-shayan1.vercel.app/app/api', {
+    const res = await axios.post(`${process.env.NEXT_API_URL}/app/api`, {
       email,
     });
       console.log("This is data",res.data); // success response
