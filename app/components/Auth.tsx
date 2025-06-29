00b27df0fedf34c4405464e82de46b7b73a135da
shayanashiq <shayanashiq8@gmil.com.com>
2025-06-29 16:17:17 +0500

    gap

--- app/components/Auth.tsx
+++ app/components/Auth.tsx
@@ -31,7 +31,7 @@ const onHandle = async () => {
           <div className="w-[800px] h-[550px] bg-gray-100 md:bg-gray-200 float-end">
           </div>
           <div className=" md:grid md:grid-cols-2 md:gap-28 absolute top-20 md:top-6">
-              <div className="bg-transparent px-3 md:px-28 md:py-20 h-[700px] mt-20 items-center">
+              <div className="bg-transparent px-3 md:px-32 md:py-20 h-[700px] mt-20 items-center">
                 <h1 className="text-2xl md:text-3xl font-bold">
                   kikk.io
                 </h1>
@@ -41,7 +41,7 @@ const onHandle = async () => {
                 <p className="mt-5 text-sm">
                   Coming soon, join waitlist to get beta access
                 </p>
-                <div className="mt-8 flex gap-1 shadow-2xl  bg-red-300 w-[350px] md:p-2 md:px-0">
+                <div className="mt-8 flex gap-2 md:gap-1 shadow-2xl  bg-red-300 w-[350px] md:p-2 py-1">
                   <input 
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
